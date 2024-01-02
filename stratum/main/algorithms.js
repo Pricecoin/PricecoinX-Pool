7563fd39f157ad6503d6e6bdc18c4147b74283ad
ZachPrice <zach.price105@gmail.com>
2024-01-01 22:39:47 -0600

    algo change

--- stratum/main/algorithms.js
+++ stratum/main/algorithms.js
@@ -5,13 +5,13 @@ const hashing = require('bindings')('hashing.node');
 // Main Algorithms Function
 const Algorithms = {
 
-  // Scrypt Algorithm
-  'scrypt': {
-    multiplier: Math.pow(2, 16),
+  // Allium Algorithm
+  'allium': {
+    multiplier: Math.pow(2, 8),
     diff: parseInt('0x00000000ffff0000000000000000000000000000000000000000000000000000'),
-    hash: function() {
-      return function(data) {
-        return hashing.scrypt.apply(this, [data, 1024, 1]);
+    hash: function(){
+      return function(){
+        return hashing.allium.apply(this, arguments);
       };
     }
   },
